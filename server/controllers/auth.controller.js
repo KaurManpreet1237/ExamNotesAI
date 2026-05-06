2f0d624fe8aca4dae4f44b413a80bbecb6001d46
Manpreet Kaur <preetkaur4715@gmail.com>
2026-05-06 13:19:24 +0530

    update the cokkie issue

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -28,6 +28,7 @@ const setTokenCookie = (res, token) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
